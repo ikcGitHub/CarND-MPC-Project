b9455acfa07b98f8a9e1569534010659a1a0f81e
ikcGitHub <ljcikc@gmail.com>
2018-06-26 21:36:05 -0400

    Lower state cost calculation

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -55,8 +55,8 @@ class FG_eval {
     // Add state cost
     // The part of the cost based on the reference state.
     for (unsigned int t = 0; t < N; t++) {
-      fg[0] += 2500 * CppAD::pow(vars[cte_start  + t], 2);
-      fg[0] += 2500 * CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 1500 * CppAD::pow(vars[cte_start  + t], 2);
+      fg[0] += 1500 * CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start    + t], 2);
     }
     
