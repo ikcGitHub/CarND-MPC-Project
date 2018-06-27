2b4515fe703561c37a7822ed9d59af61e45802a7
ikcGitHub <ljcikc@gmail.com>
2018-06-26 21:49:14 -0400

    Tune parameter

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -70,7 +70,7 @@ class FG_eval {
     // Add value gap cost
     // Minimize the value gap between sequential actuations.
     for (unsigned int t = 0; t < N - 2; t++) {
-      fg[0] += 3000 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 300000 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += 3000 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
    
