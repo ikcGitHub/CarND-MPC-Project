a3d4b52157fc6222e98e5a29426b5c0a90834e48
ikcGitHub <ljcikc@gmail.com>
2018-06-26 21:52:12 -0400

    Lower state and actuator cost calculation

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -55,16 +55,16 @@ class FG_eval {
     // Add state cost
     // The part of the cost based on the reference state.
     for (unsigned int t = 0; t < N; t++) {
-      fg[0] += 1500 * CppAD::pow(vars[cte_start  + t], 2);
-      fg[0] += 1500 * CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 1000 * CppAD::pow(vars[cte_start  + t], 2);
+      fg[0] += 1000 * CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start    + t], 2);
     }
     
     // Add actuator cost
     // Minimize the use of actuators.
     for (unsigned int t = 0; t < N - 1; t++) {
-      fg[0] += 100 * CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += 100 * CppAD::pow(vars[a_start     + t], 2);
+      fg[0] += 50 * CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 50 * CppAD::pow(vars[a_start     + t], 2);
     }
     
     // Add value gap cost
